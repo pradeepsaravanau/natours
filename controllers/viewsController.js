f9d94aab4756c20d4973e3933482af2a7fb1bd0b
Pradeepsaravana u <pradsara8@gmail.com>
2022-02-28 22:10:19 +0530

    changes"

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -23,6 +23,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
     path: 'reviews',
     fields: 'review rating user'
   });
+  console.log(tour);
   if (!tour) {
     return next(new AppError('There is no tour with that name', 404));
   }
