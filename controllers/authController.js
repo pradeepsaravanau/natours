0bc2069e26e6848af0d1e8021ee5d93c1cd2a322
Pradeepsaravana u <pradsara8@gmail.com>
2022-03-01 18:32:35 +0530

    url fixes

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,7 +49,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     confirmEmailToken: token
   });
   console.log(req.get('host'));
-  const url = `/${newUser.email}`;
+  const url = `${req.protocol}/${newUser.email}`;
   await new Email(newUser, url).sendEmailConfirmation();
   // createSendToken(newUser, 201, res);
   res.status(200).json({
